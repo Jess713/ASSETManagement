cee4d3939be7f9b4e34802dbd43ceecccb130a6f
Jae Hyunhyo <jaehh1987@gmail.com>
2020-04-08 10:39:09 -0700

    Setup + navigation bar tests

---
+++ NUnitTestProject1/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Test
+{
+    class Customer
+    {
+    }
+}
